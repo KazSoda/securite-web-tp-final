9b0b9024715fa63a1345af75cbf1f603c9adf168
Sôda <92747405+KazSoda@users.noreply.github.com>
2026-01-07 08:55:21 +0100

    fix user auth

--- vulnerable/backend/routes/users.js
+++ vulnerable/backend/routes/users.js
@@ -43,7 +43,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // Route pour supprimer un utilisateur
-router.delete('/:id', async (req, res) => {
+router.delete('/:id',  authenticate, authorizeAdmin, async (req, res) => {
   const { id } = req.params;
   const sql = 'DELETE FROM users WHERE id = ?';
   try {
@@ -56,7 +56,7 @@ router.delete('/:id', async (req, res) => {
 });
 
 // Route pour modifier un utilisateur
-router.put('/:id', async (req, res) => {
+router.put('/:id', authenticate, authorizeAdmin,  async (req, res) => {
   const { id } = req.params;
   const { username, email, password, role } = req.body;
   const sql = 'UPDATE users SET username = ?, email = ?, password = ?, role = ? WHERE id = ?';
